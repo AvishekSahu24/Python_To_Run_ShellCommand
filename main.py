36f6f45a5536726577ed31a5734cc6d2a4a412b1
AvishekSahu24 <avisahu247@gmail.com>
2019-12-23 12:55:17 +0530

    pytho file to run shell commands

---
+++ main.py
@@ -0,0 +1,7 @@
+import os
+os.system("ls -l")
+os.system("sudo apt-get update")
+os.system("===========================================================")
+os.system("Update Completed")
+os.system("===========================================================")
+os.system("sudo apt-get upgrade")
